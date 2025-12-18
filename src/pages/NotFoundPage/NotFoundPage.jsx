68f46315028c50988e3b70d267da243f0b1ad8ff
Rotem Harduf <159410654+rotemharduf1@users.noreply.github.com>
2025-12-18 18:17:45 +0200

    Force PageTitle casing fix (Windows/Git issue)

--- src/pages/NotFoundPage/NotFoundPage.jsx
+++ src/pages/NotFoundPage/NotFoundPage.jsx
@@ -1,4 +1,4 @@
-import PageTitle from "../../components/pageTitle/pageTitle";
+import PageTitle from "../../components/PageTitle/PageTitle";
 
 export default function NotFoundPage() {
     return (
